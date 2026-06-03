259383ab2a65191c93837771991d95bd4aff2200
arvi9 <adc9work@gmail.com>
2026-06-03 16:38:26 +0530

    Add ElectricCar and GasCar classes inheriting Vehicle

--- OOP/Inheritance.cs
+++ OOP/Inheritance.cs
@@ -26,3 +26,36 @@ public class Vehicle
         Console.WriteLine($"{Year} {Make} {Model}");
     }
 }
+
+
+public class ElectricCar : Vehicle
+{
+    private int _batteryCapacity;
+
+    public ElectricCar(string make, string model, int year, int batteryCapacity)
+        : base(make, model, year)
+    {
+        _batteryCapacity = batteryCapacity;
+    }
+
+    public void ChargeBattery()
+    {
+        Console.WriteLine($"Charging {_batteryCapacity}kWh battery");
+    }
+}
+
+public class GasCar : Vehicle
+{
+    private double _fuelTankSize;
+
+    public GasCar(string make, string model, int year, double fuelTankSize)
+        : base(make, model, year)
+    {
+        _fuelTankSize = fuelTankSize;
+    }
+
+    public void FillTank()
+    {
+        Console.WriteLine($"Filling {_fuelTankSize}L fuel tank");
+    }
+}
